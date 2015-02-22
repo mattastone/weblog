d9f188e4cc1f5f7235ffe261165308f27dac39fb
Matt Stone <mattastone@gmail.com>
2015-02-21 19:15:05 -0600

    Post model with tests

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
-class Poll(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
+    text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
